117f32767af401df5912711a1b1c784b96d2c7e1
vitamin-software <erkin.kanlioglu+vs@gmail.com>
2017-04-09 22:27:13 +0100

    Added TODO file

---
+++ SparkWithScala/src/io/vitamin/spark/MovieSimiliaritiesTODO.scala
@@ -0,0 +1,16 @@
+package io.vitamin.spark
+
+/**
+  * Created by vitamin on 09/04/2017.
+  */
+object MovieSimiliaritiesTODO {
+    /*
+          Implement Pearson or Jaccard similarities
+          Nuke bad scores
+          Play around with thresholds for minimum score, minimum numbers of pairs -
+          try weighted similarities
+          Use other information in the u.items file.
+
+     */
+
+}
